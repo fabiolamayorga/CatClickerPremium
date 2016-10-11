215fd1dcaa28a128d8d044367266cc1cc04f6aad
Fabiola Mayorga <fab.tames@gmail.com>
2016-10-10 21:30:47 -0600

    Added new view

--- js/catClicker.js
+++ js/catClicker.js
@@ -4,27 +4,27 @@ $(function(){
 			this.data = [
 				{
 					name: 'Cat1',
-					img: "img/cat1",
+					img: "img/cat1.jpg",
 					clickCounter: 0,
 				},
 				{
 					name: 'Cat2',
-					img: "img/cat2",
+					img: "img/cat2.jpg",
 					clickCounter: 0,
 				},
 				{
 					name: 'Cat3',
-					img: "img/cat3",
+					img: "img/cat3.jpg",
 					clickCounter: 0,
 				},
 				{
 					name: 'Cat4',
-					img: "img/cat4",
+					img: "img/cat4.jpg",
 					clickCounter: 0,
 				},
 				{
 					name: 'Cat5',
-					img: "img/cat5",
+					img: "img/cat5.jpg",
 					clickCounter: 0,
 				}
 			];
@@ -44,27 +44,47 @@ $(function(){
             model.init();
             catListView.init();
             catView.init();
+            catListItemView.init();
         },
 
-        increaseCounter: function() {
+        /*increaseCounter: function() {
 
         },
 
         clickFunction: function(currentCat) {
 
-        }
+        }*/
+
+        initEvents: function() {
+
+        },
 	};
 
 	var catView = {
 		init: function() {
 			this.catView = $(".catView");
-			this.counter = $(".counter");
+			this.counter = $(".catCounter");
 			this.render();
 		},
 
 		render: function() {
-			var currentCat = catListView.getCurrentCat;
-			console.log(currentCat);
+			var currentCat = catListView.getCurrentCat(),
+				currentCatImg = "",
+				currentCatclickCounter = 0;
+				htmlStrImg = '';
+				htmlStrCounter = '';
+
+			octopus.getCats().forEach(function(cats,index){
+				if (index === currentCat) {
+					currentCatImg= cats.img;
+					currentCatclickCounter = cats.clickCounter;
+				}
+			}); //Retrieving all cats to compare which one matches the current cat
+
+			htmlStrImg += '<img src="' + currentCatImg + '"/>';
+
+			this.catView.html(htmlStrImg);
+			this.counter.html(currentCatclickCounter);
 		}
 	};
 
@@ -84,18 +104,56 @@ $(function(){
             		htmlStr += '<li class="catItem currentCat">'+
                         cats.name +
                     '</li>';
-            	}
+            	}else {
 
                 htmlStr += '<li class="catItem">'+
                         cats.name +
                     '</li>';
+                }
             });
             this.catList.html(htmlStr);
+            this.setCurrentCat(currentCat);
 		},
 
 		getCurrentCat: function() {
-			return currentCat;
+			return parseInt(this.currentCat);
+		},
+
+		setCurrentCat: function(currentCat) {
+			this.currentCat = currentCat;
 		}
+	};
+
+	var catListItemView = {
+		init: function() {
+			var listItem = document.getElementsByTagName('li');
+			console.log(listItem);
+
+			for (i = 0; i < listItem.length; i++) { // we do this to add the even to each list item
+				listItem[i].addEventListener('click',this.clickFunction, false);
+			}
+
+		},
+
+        clickFunction: function(e) {
+        	var self = this;
+        	console.log("click", this);
+        	console.log(self.className);
+
+			self.classList.remove("currentCat");
+			self.classList.add("currentCat");
+
+        	if (self.className === "catItem currentCat") {
+        		console.log("currentCat");
+        	}
+
+        },
+
+
+        increaseCounter: function() {
+
+        },
+
 	};
 
 	octopus.init();
